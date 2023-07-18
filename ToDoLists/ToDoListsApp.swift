f032f58f2c2898694144173e10092e12cb29a54b
Hillary <hillarytrinh8@gmail.com>
2023-07-18 10:44:43 -0400

    Initial Commit

---
+++ ToDoLists/ToDoListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListsApp.swift
+//  ToDoLists
+//
+//  Created by Melanie Trinh on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
